0cabf7d3843294edc562caadb6fc316d6088eb82
t0ri <victoriarosemurray@gmail.com>
2019-01-31 15:51:21 -0800

    Users can submit and save posts

--- server.js
+++ server.js
@@ -4,16 +4,31 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars');
 
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+
 // Connect to DB
+require('./data/reddit-db');
 
 // Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
 // Routes
+require('./controllers/posts.js')(app);
+
 app.get('/', (req, res) => {
-    res.render('index')
-});
+    res.render('posts-new')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+})
 
 // Server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Reddit.js listening on port ${port}!`))
