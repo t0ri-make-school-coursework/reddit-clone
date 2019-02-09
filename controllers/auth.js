c51c24715d56cf435d33d4f0790626ea7f78d849
t0ri <victoriarosemurray@gmail.com>
2019-02-09 10:27:19 -0800

    Users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,4 +23,10 @@ module.exports = (app) => {
       return res.status(400).send({ err: err });
     });
   });
+
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
+  });
 }
