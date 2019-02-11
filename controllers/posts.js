471396b238baa47cace1bf57397b89677eb03a97
t0ri <victoriarosemurray@gmail.com>
2019-02-11 09:28:59 -0800

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,17 +1,21 @@
-const Post = require('../models/post');
+const Post = require('../models/post')
+const User = require('../models/user')
+
 
 module.exports = app => {
-  app.get("/", (req, res) => {
+  // INDEX
+  app.get('/', (req, res) => {
     var currentUser = req.user;
-  
-    Post.find({})
-      .then(posts => {
-        res.render("posts-index", { posts, currentUser });
-      })
-      .catch(err => {
+    // res.render('home', {});
+    console.log(req.cookies);
+    Post.find().populate('author')
+    .then(posts => {
+        res.render('posts-index', { posts, currentUser });
+        // res.render('home', {});
+    }).catch(err => {
         console.log(err.message);
-      });
-  });
+    })
+})
 
   app.get('/posts/new', (req, res) => {
     var currentUser = req.user;
@@ -22,36 +26,51 @@ module.exports = app => {
   // CREATE
   app.post("/posts/new", (req, res) => {
     if (req.user) {
-      var post = new Post(req.body);
+        var post = new Post(req.body);
+        post.author = req.user._id;
 
-      post.save(function(err, post) {
-        return res.redirect(`/`);
-      });
+        post
+            .save()
+            .then(post => {
+                return User.findById(req.user._id);
+            })
+            .then(user => {
+                user.posts.unshift(post);
+                user.save();
+                // REDIRECT TO THE NEW POST
+                res.redirect(`/posts/${post._id}`);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
     } else {
-      return res.status(401); // UNAUTHORIZED
+        return res.status(401); // UNAUTHORIZED
     }
-  });
+});
 
-  app.get('/posts/:id', function(req, res) {
-    var currentUser = req.user;
-    // LOOK UP THE POST
-    Post.findById(req.params.id).populate('comments').then((post) => {
-      res.render('posts-show', { post })
-    }).catch((err) => {
-      console.log(err.message)
-    })
-  });
+app.get("/posts/:id", function (req, res) {
+  var currentUser = req.user;
+  // LOOK UP THE POST
+
+  Post.findById(req.params.id).populate('comments').populate('author')
+      .then(post => {
+          res.render("posts-show", { post, currentUser });  
+      })
+      .catch(err => {
+          console.log(err.message);
+      });
+});
 
   // SUBREDDIT
-  app.get("/n/:subreddit", function(req, res) {
-    var currentUser = req.user;
-    Post.find({ subreddit: req.params.subreddit })
-    .then(posts => {
-      res.render("posts-index", { posts });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  });
+app.get("/n/:subreddit", function (req, res) {
+  var currentUser = req.user;
+  Post.find({ subreddit: req.params.subreddit }).populate('author')
+      .then(posts => {
+          res.render("posts-index", { posts, currentUser });
+      })
+      .catch(err => {
+          console.log(err);
+      });
+});
 };
 
--- models/post.js
+++ models/post.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+  author : { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true },
